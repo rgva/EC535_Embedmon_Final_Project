09521832ba17b93fc4a271518c53239e7156e23d
Roman Velez-Alicea <113293032+rgva@users.noreply.github.com>
2023-04-18 16:22:57 -0400

    Add files via upload
    First-pass at turn-based fighting game

---
+++ FinalProject/widget.h
@@ -0,0 +1,105 @@
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QRandomGenerator>
+/*Sprite includes*/
+#include <QLabel>
+#include <QPixmap>
+#include <QTimer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+/* This class lists out all possible actions in a move */
+enum class Action {
+    None,
+    LightAttack,
+    StrongAttack,
+    Block,
+    Potion
+};
+
+/* This class defines character attributes */
+class Character {
+public:
+    int health = 100;
+    int healthPotion = 1;
+    Action currentAction = Action::None;
+
+    void setAction(Action action) {
+        currentAction = action;
+    }
+
+    int lightAttack() {
+        if (QRandomGenerator::global()->generateDouble() < 0.1) return 0; // 10% miss chance
+        return QRandomGenerator::global()->bounded(8, 13); // 8-12 damage
+    }
+
+    int strongAttack() {
+        if (QRandomGenerator::global()->generateDouble() < 0.5) return 0; // 50% miss chance
+        return QRandomGenerator::global()->bounded(15, 26); // 15-25 damage
+    }
+
+    bool block() {
+        return QRandomGenerator::global()->generateDouble() < 0.5; // 50% chance to increase miss chance
+    }
+
+    int useHealthPotion() {
+        if (healthPotion > 0) {
+            healthPotion--;
+            return 25;
+        }
+        return 0;
+    }
+};
+
+
+class Widget : public QWidget
+
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+    /*Declare characters*/
+    Character player1;
+    Character player2;
+    /*Character actions*/
+    Action player1Action; //Player 1's action
+    Action player2Action; //Player 2's action
+    /*Slot functions for actions */
+    void lattackP1_clicked();
+    void sattackP1_clicked();
+    void blockP1_clicked();
+    void potionP1_clicked();
+    void lattackP2_clicked();
+    void sattackP2_clicked();
+    void blockP2_clicked();
+    void potionP2_clicked();
+    /*Game mechanic functions*/
+    void checkBothPlayersReady();
+    void executeRound();
+    void updateHealthBars();
+    void checkGameOver();
+    /*Sprite functions*/
+    QLabel *player1Sprite;
+    QLabel *player2Sprite;
+    //Establishes delays between sprite frames
+    QTimer *animationTimer;
+    //Initialize the player sprite frames
+    int player1Frame;
+    int player2Frame;
+
+private slots:
+    //Function to update sprites
+    void updateSprites();
+};
+
+#endif // WIDGET_H
